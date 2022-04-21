a9e092fdc7fdbdf1679c8a3010baace88ace9c68
Nofar <nofar11sh@gmail.com>
2022-04-21 19:39:11 +0300

    [WIP] get user accounts abd banks

--- moneymaly-app/src/components/Home.js
+++ moneymaly-app/src/components/Home.js
@@ -1,50 +1,49 @@
 import axios from 'axios';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
 
-class Home extends Component {
-    state = {};
+export default function Home() {
+    const [state, setState] = useState({
+        userData: null
+    });
 
-    componentDidMount() {
+    const renderUserNotLoggedIn = (
+        <Container component="main" maxWidth="xs">
+            <h4 className="center">Home</h4>
+            <h5 className="center">You are not logged in!</h5>
+            <h5 className="center">Please Login First <NavLink to="/Login">Login</NavLink></h5>
+        </Container>
+    );
+    const GetUserDataFromServer = () => {
+        axios.get('http://192.116.98.107:8081/users', {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
+            },
+            params: { username: localStorage.getItem('username') }
+        }).then(
+            res => {
+                setState(prevState => ({ ...prevState, userData: res.data }));
+            },
+            err => {
+                console.log(err);
+            }
+        );
+    };
+    useEffect(() => {
         if (localStorage.getItem("token") && localStorage.getItem('username')) {
-            axios.get('http://192.116.98.107:8081/users', {
-                headers: {
-                    "Authorization": `Bearer ${localStorage.getItem("token")}`
-                },
-                params: {
-                    username: localStorage.getItem('username')
-                }
-            }).then(
-                res => {
-                    this.setState({ data: res.data });
-                },
-                err => {
-                    console.log(err);
-                }
-            );
+            GetUserDataFromServer();
         }
-    }
-
-    render() {
-        if (this.state.data) {
-            return (
-                <div className="container">
+    });
+    return (
+        state.userData ?
+            (
+                <Container component="main" maxWidth="xs">
                     <h4 className="center">Home</h4>
-                    <h2>Hey: {this.state.data.full_name}</h2>
-                </div>
-            )
-        }
-        else {
-            return (
-                <div className="container">
-                    <h4 className="center">Home</h4>
-                    <h5 className="center">you are not logged in !</h5>
-                    <h5 className="center">Please Login First <NavLink to="/Login">Login</NavLink></h5>
-
-                </div>
-            )
-        }
-    }
-}
-
-export default Home;
+                    <h2>full_name: {state.userData.full_name}</h2>
+                </Container>
+            ) :
+            (
+                renderUserNotLoggedIn
+            ));
+}; 
