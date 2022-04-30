74a1d466a17374eeb35ccb362cf22c5ee4f67994
Nofar <nofar11sh@gmail.com>
2022-05-01 00:44:16 +0300

    Fix moneymaly title position

--- moneymaly-app/src/components/Navbar.js
+++ moneymaly-app/src/components/Navbar.js
@@ -46,12 +46,10 @@ const useStyles = makeStyles((theme) => ({
         flexGrow: 1
     },
     menuButton: {
-        marginRight: theme.spacing(2),
+        marginRight: theme.spacing(2)
     },
     title: {
-        alignItems: 'center',
-        flex: 1,
-        justifyContent: 'center'
+        marginRight: theme.spacing(1),
     },
     search: {
         position: 'relative',
